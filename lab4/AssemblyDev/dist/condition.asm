7cbabe21dddefaf15b6d1b388837b0ab74f9bfee
LolaInTa Chi <lolainta@gmail.com>
2023-11-03 13:41:56 +0800

    feat: lab4 real cond

--- lab4/AssemblyDev/dist/condition.asm
+++ lab4/AssemblyDev/dist/condition.asm
@@ -1,4 +1,21 @@
 mov eax, dword [rsp+0x0]
-mov ebx, dword [rsp+0x8]
+mov ebx, dword [rsp+0x4]
+cmp eax, ebx
+jge PA
+mov eax, dword [rsp+0x4]
+PA:
 mov ecx, dword [rsp+0x8]
+mov edx, dword [rsp+0xc]
+mov ebx, ecx
+cmp ecx, edx
+jb PB
+mov ebx, edx
+PB:
+mov edx, ecx
+and edx, 0x1
+jnz PC
 imul ecx, 4
+jmp final
+PC:
+sar ecx, 0x3
+final:
