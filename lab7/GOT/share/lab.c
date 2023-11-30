61a877012369d58428e6e6c36d0a83fb11e35227
LolaInTa Chi <lolainta@gmail.com>
2023-12-01 01:05:11 +0800

    lab7: got init

---
+++ lab7/GOT/share/lab.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<sys/mman.h>
+#include<unistd.h>
+unsigned long long arr[1] = {1234};
+int main(){
+    setvbuf(stdin, 0, _IONBF, 0);
+    setvbuf(stdout, 0, _IONBF, 0);
+    int index;
+    printf("idx: ");
+    scanf("%d", &index);
+    printf("arr[%d] = %lu\n", index, arr[index]);
+    printf("val: ");
+    scanf("%lu", &arr[index]);
+    printf("/bin/sh");
+	return 0;
+}
